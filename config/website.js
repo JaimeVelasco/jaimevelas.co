43015796f0ee0214c4bd92259c9b23f137133cb3
JaimeVelasco <contact@jaimevelas.co>
2019-07-03 14:39:24 -0500

    delete old vue files and configure site settings

--- config/website.js
+++ config/website.js
@@ -3,21 +3,21 @@ const tailwind = require('../tailwind')
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Cara - Gatsby Starter Portfolio', // Navigation and Site Title
-  siteTitleAlt: 'Cara', // Alternative Site title for SEO
-  siteTitleShort: 'Cara', // short_name for manifest
+  siteTitle: 'Jaime Velasco', // Navigation and Site Title
+  siteTitleAlt: 'Web Developer', // Alternative Site title for SEO
+  siteTitleShort: 'JaimeVelas', // short_name for manifest
   siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
-  siteUrl: 'https://cara.lekoarts.de', // Domain of your site. No trailing slash!
+  siteUrl: 'http://jaimevelas.co', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logo.png', // Used for SEO and manifest
   siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
-  author: 'LekoArts', // Author for schema.org JSONLD
+  author: 'Jaime Velasco', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@cara', // Twitter Username
-  ogSiteName: 'cara', // Facebook Site Name
+  userTwitter: '@JaimeVelas', // Twitter Username
+  // ogSiteName: '', // Facebook Site Name
   ogLanguage: 'en_US', // Facebook Language
-  googleAnalyticsID: 'UA-47519312-5',
+  googleAnalyticsID: 'UA-143265635-1',
 
   // Manifest and Progress color
   themeColor: tailwind.colors.orange,
